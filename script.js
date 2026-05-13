dfe6a01461c8a0d816728d1e42a7ae6f0650fe2a
saxenavansh-cs <saxenavanshvs30@gmail.com>
2026-05-13 10:35:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-const GOOGLE_API_KEY = "YOUR_API_KEY";
-const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
+const GOOGLE_API_KEY = "AIzaSyC6_GSSUR6goBM9-SbhCw4m0GIl6SfICMk";
+
+const API_REQUEST_URL =
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
 const loadSavedChatHistory = () => {
